e6929ebff118d242f66c8c7d80c097fc551bc90f
optifat <pavsmel@mail.ru>
2020-10-12 21:56:06 +0300

    now head is fixed and tail is moving

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -1,7 +1,10 @@
 use std::fmt;
+use std::rc::Rc;
+use std::cell::RefCell;
 
-type Link<T> = Option<Box<T>>;
+type Link<T> = Option<Rc<RefCell<T>>>;
 
+#[derive(Clone)]
 struct Node<T> {
     data: T,
     next: Link<Node<T>>,
@@ -9,6 +12,7 @@ struct Node<T> {
 
 pub struct List<T>{
     head: Link<Node<T>>,
+    tail: Link<Node<T>>,
     size: usize
 }
 
@@ -18,25 +22,41 @@ impl<T: fmt::Debug> Node<T>{
     }
 }
 
-impl<T: fmt::Debug> List<T>{
+impl<T: fmt::Debug+std::clone::Clone> List<T>{
     pub fn new() -> List<T>{
-        List{head: None, size: 0}
+        List{head: None, tail: None, size: 0}
     }
 
     pub fn push_back(&mut self, data: T){
         println!("Adding new element {:?}", data);
-        let new_node = Box::new(Node::new(data, self.head.take()));
-        self.head = Some(new_node);
-        self.size += 1;
+        let new_node = Rc::new(RefCell::new(Node::new(data, None)));
+        if self.size == 0{
+            self.head = Some(new_node);
+            self.tail = Some(Rc::clone(self.head.as_ref().unwrap()));
+            self.size += 1;
+        }
+        else{
+            //println!("{:?}", Rc::ptr_eq(self.head.as_ref().unwrap(), self.tail.as_ref().unwrap()));
+            {
+                let mut ref_tail = self.tail.as_ref().unwrap().borrow_mut();
+                ref_tail.next.replace(new_node);
+
+            }
+            self.tail = Some(Rc::clone(self.tail.as_ref().unwrap()));
+            //println!("{:?}", Rc::ptr_eq(self.head.as_ref().unwrap(), self.tail.as_ref().unwrap()));
+            self.size += 1;
+        }
     }
 
     pub fn print(&self){
-        //let mut printing_stack = stack::Stack::<T>::new(self.size);
-        println!("Printing list in reverse order");
-        let mut current_node = self.head.as_ref();
+        println!("Printing from first element");
+        let mut current_node = self.head.clone();
         for _ in 0..self.size{
-            print!("{:?} ", current_node.unwrap().data);
-            current_node = current_node.unwrap().next.as_ref();
+            current_node = {
+                let current_node_borrowed = current_node.as_ref().unwrap().borrow();
+                print!("{:?} ", current_node_borrowed.data);
+                current_node_borrowed.next.clone()
+            };
         }
         println!("");
     }
