0d37b82b3c10baf6e7ba78fd1e1dff8b20f181ad
optifat <pavsmel@mail.ru>
2020-10-12 15:34:50 +0300

    working version with unique ptrs

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -1,7 +1,4 @@
 use std::fmt;
-use std::ptr;
-
-use crate::data_structures::stack;
 
 type Link<T> = Option<Box<T>>;
 
@@ -27,53 +24,20 @@ impl<T: fmt::Debug> List<T>{
     }
 
     pub fn push_back(&mut self, data: T){
+        println!("Adding new element {:?}", data);
         let new_node = Box::new(Node::new(data, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
+
     pub fn print(&self){
         //let mut printing_stack = stack::Stack::<T>::new(self.size);
-        let mut current_node = self.head.as_ref().unwrap();
+        println!("Printing list in reverse order");
+        let mut current_node = self.head.as_ref();
         for _ in 0..self.size{
-            print!("{:?} ", current_node.data);
-            //current_node = current_node.next.as_ref().unwrap();
+            print!("{:?} ", current_node.unwrap().data);
+            current_node = current_node.unwrap().next.as_ref();
         }
-        print!("{:?}", self.head.as_ref().unwrap().next.as_ref().unwrap().data);
         println!("");
     }
-    /*
-    pub fn add(&mut self, data: T){
-        if self.size == 0{
-            self.head = &mut Node::new(data, ptr::null_mut());
-            self.tail = self.head;
-            self.size += 1;
-        }
-        else{
-            unsafe{
-                (*self.tail).next =  &mut Node::new(data, self.tail);
-                self.tail = (*self.tail).next;
-                //println!("{:?}", (*self.tail).data);
-            }
-            self.size += 1
-        }
-    }
-
-    pub fn pop(&mut self){
-        unsafe{
-            self.tail = (*self.tail).prev;
-        }
-        self.size -= 1;
-    }
-
-    pub fn print(&self){
-        let mut current_node = self.head;
-        unsafe{
-            for _ in 0..self.size{
-                print!("{:?} ", (*current_node).data);
-                current_node = (*current_node).next;
-            }
-            println!("");
-        }
-    }
-    */
 }
