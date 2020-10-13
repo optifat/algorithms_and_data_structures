ed005e9161c98b6da30e74aa665c8eea770f7a4b
optifat <pavsmel@mail.ru>
2020-10-13 14:11:17 +0300

    fixed wrong tail cloning in push_back + made list double linked

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -4,10 +4,10 @@ use std::cell::RefCell;
 
 type Link<T> = Option<Rc<RefCell<T>>>;
 
-#[derive(Clone)]
 struct Node<T> {
     data: T,
     next: Link<Node<T>>,
+    prev: Link<Node<T>>
 }
 
 pub struct List<T>{
@@ -17,33 +17,35 @@ pub struct List<T>{
 }
 
 impl<T: fmt::Debug> Node<T>{
-    fn new(data: T, next: Link<Node<T>>) -> Node<T>{
-        Node{data, next}
+    fn new(data: T, next: Link<Node<T>>, prev: Link<Node<T>>) -> Node<T>{
+        Node{data, next, prev}
     }
 }
 
-impl<T: fmt::Debug+std::clone::Clone> List<T>{
+impl<T: fmt::Debug> List<T>{
     pub fn new() -> List<T>{
         List{head: None, tail: None, size: 0}
     }
 
     pub fn push_back(&mut self, data: T){
         println!("Adding new element {:?}", data);
-        let new_node = Rc::new(RefCell::new(Node::new(data, None)));
         if self.size == 0{
+            let new_node = Rc::new(RefCell::new(Node::new(data, None, None)));
             self.head = Some(new_node);
             self.tail = Some(Rc::clone(self.head.as_ref().unwrap()));
             self.size += 1;
         }
         else{
-            //println!("{:?}", Rc::ptr_eq(self.head.as_ref().unwrap(), self.tail.as_ref().unwrap()));
+            let new_node = Rc::new(RefCell::new(Node::new(data, None, self.tail.clone())));
             {
                 let mut ref_tail = self.tail.as_ref().unwrap().borrow_mut();
                 ref_tail.next.replace(new_node);
 
             }
-            self.tail = Some(Rc::clone(self.tail.as_ref().unwrap()));
-            //println!("{:?}", Rc::ptr_eq(self.head.as_ref().unwrap(), self.tail.as_ref().unwrap()));
+            let tmp = {
+                Some(Rc::clone(self.tail.as_ref().unwrap().borrow().next.as_ref().unwrap()))
+            };
+            self.tail = tmp.clone();
             self.size += 1;
         }
     }
